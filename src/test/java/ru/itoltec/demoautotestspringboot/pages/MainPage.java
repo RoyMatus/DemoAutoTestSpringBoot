0dda320fcee40ef0c343d3e97b79351d72c35335
Roy Matus <piligrim18@gmail.com>
2024-08-09 16:56:40 +0300

    Click "Accept cookies" button automatically if exists

--- src/test/java/ru/itoltec/demoautotestspringboot/pages/MainPage.java
+++ src/test/java/ru/itoltec/demoautotestspringboot/pages/MainPage.java
@@ -1,9 +1,11 @@
 package ru.itoltec.demoautotestspringboot.pages;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 
 
 @Component
@@ -15,12 +17,24 @@ public class MainPage extends BasePage {
     @FindBy(xpath = "//div[@class=\"tnb-right-section\"]/a[@title=\"Login to your account\"]")
     private WebElement btnLogin;
 
+    @FindBy(xpath = "//*[@id='accept-choices']")
+    private WebElement btnAcceptCookies;
+
     public void clickLoginBtn() {
         btnLogin.click();
     }
 
     public void open() {
         navigateTo(mainPageUrl);
+        clickAcceptCookiesBtn();
+    }
+
+    private void clickAcceptCookiesBtn() {
+        try {
+            btnAcceptCookies.click();
+        } catch (NoSuchElementException e){
+            System.out.println("Accepted cookies button is not displayed.");
+        }
     }
 
 }
